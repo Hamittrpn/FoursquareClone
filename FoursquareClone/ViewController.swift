e364228d1847a50a9358bfdd506ceead4adccde8
Hamit Tırpan <hamittirpan@gmail.com>
2019-10-11 15:46:46 +0300

    Initial Commit

---
+++ FoursquareClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Hamit  Tırpan on 11.10.2019.
+//  Copyright © 2019 Hamit  Tırpan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
