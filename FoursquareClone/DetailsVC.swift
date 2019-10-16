a2ef1379eaf46e19650892872948af80424ea901
Hamit Tırpan <hamittirpan@gmail.com>
2019-10-16 12:06:33 +0300

    Added Navigation

--- FoursquareClone/DetailsVC.swift
+++ FoursquareClone/DetailsVC.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import MapKit
+import Parse
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var placeImageView: UIImageView!
     @IBOutlet weak var placeNameLabel: UILabel!
@@ -17,11 +18,138 @@ class DetailsVC: UIViewController {
     @IBOutlet weak var placeDescriptionLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
     
+    var chosenPlaceId = ""
+    var chosenLatitude = Double()
+    var chosenLongitude = Double()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        getDataFromParse()
+        
+        mapView.delegate = self
     }
     
-
+    func getDataFromParse(){
+        
+        let query = PFQuery(className: "Places")
+        query.whereKey("objectId", equalTo: chosenPlaceId)
+        query.findObjectsInBackground { (objects, error) in
+            if error != nil {
+                self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+            } else {
+                if objects != nil {
+                    if objects!.count > 0 {
+                        let chosenPlaceObject = objects![0]
+                        
+                        if let placeName = chosenPlaceObject.object(forKey: "name") as? String{
+                            self.placeNameLabel.text = placeName
+                        }
+                        if let placeType = chosenPlaceObject.object(forKey: "type") as? String{
+                            self.placeTypeLabel.text = placeType
+                        }
+                        if let placeDescription = chosenPlaceObject.object(forKey: "description") as? String{
+                            self.placeDescriptionLabel.text = placeDescription
+                        }
+                        if let placeLatitude = chosenPlaceObject.object(forKey: "latitude") as? String{
+                            if let placeLatitudeDouble = Double(placeLatitude){
+                                self.chosenLatitude = placeLatitudeDouble
+                            }
+                        }
+                        if let placeLongitude = chosenPlaceObject.object(forKey: "longitude") as? String{
+                            if let placeLongitudeDouble = Double(placeLongitude) {
+                                self.chosenLatitude = placeLongitudeDouble
+                            }
+                        }
+                        if let imageData = chosenPlaceObject.object(forKey: "image") as? PFFileObject{
+                            imageData.getDataInBackground(block: { (data, error) in
+                                if error == nil{
+                                    if data != nil{
+                                        self.placeImageView.image = UIImage(data: data!)
+                                    }
+                                } else {
+                                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                                }
+                            })
+                        }
+                    }
+                    
+                    // MAPS
+                    
+                    let location = CLLocationCoordinate2D(latitude: self.chosenLatitude, longitude: self.chosenLongitude)
+                    let span = MKCoordinateSpan(latitudeDelta: 0.035, longitudeDelta: 0.035)
+                    let region = MKCoordinateRegion(center: location, span: span)
+                    
+                    self.mapView.setRegion(region, animated: true)
+                    
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = location
+                    annotation.title = self.placeNameLabel.text
+                    annotation.subtitle = self.placeTypeLabel.text
+                    
+                    self.mapView.addAnnotation(annotation)
+                }
+            }
+        }
+    }
+    
+    // Navigasyon özelliği ekleme
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        // Eğer kullanıcının yeri ile ilgili bir anatasyon varsa bununla ilgili bir işlem yapmak istemiyorum.
+        if annotation is MKUserLocation{
+            return nil
+        }
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId)
+        
+        if pinView == nil {
+            
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.canShowCallout = true
+            let button = UIButton(type: UIButton.ButtonType.detailDisclosure)
+            pinView?.rightCalloutAccessoryView = button
+        } else {
+            pinView?.annotation = annotation
+        }
+        return pinView
+    }
+    
+    // Pin'deki butona tıklanınca ne olacak ?
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if self.chosenLatitude != 0.0 && self.chosenLongitude != 0.0 {
+            let reguestLocation = CLLocation(latitude: self.chosenLatitude, longitude: self.chosenLongitude)
+            
+            CLGeocoder().reverseGeocodeLocation(reguestLocation) { (placemarks, error) in
+                if let placemark = placemarks {
+                    
+                    if placemark.count > 0 {
+                        let mkPlaceMark = MKPlacemark(placemark: placemark[0])
+                        let mapItem = MKMapItem(placemark: mkPlaceMark)
+                        mapItem.name = self.placeDescriptionLabel.text
+                        
+                        let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+                        
+                        mapItem.openInMaps(launchOptions : launchOptions)
+                    } else {
+                        self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                    }
+                }
+            }
+        } else {
+            makeAlert(titleInput: "Error", messageInput: "Something was wrong!")
+        }
+    }
+        
+    func makeAlert(titleInput:String, messageInput:String){
+            
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
+
--- FoursquareClone/PlacesVC.swift
+++ FoursquareClone/PlacesVC.swift
@@ -9,16 +9,73 @@
 import UIKit
 import Parse
 
-class PlacesVC: UIViewController {
+class PlacesVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var placeArray = [String]()
+    var idArray = [String]()
+    var selectedPlaceId = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
         
         navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logoutButtonClicked))
+        
+        getDataFromParse()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placeArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = placeArray[indexPath.row]
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC"{
+            let destinationVC = segue.destination as? DetailsVC
+            destinationVC?.chosenPlaceId = selectedPlaceId
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPlaceId = idArray[indexPath.row]
+        self.performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    func getDataFromParse(){
+        
+        let query = PFQuery(className: "Places")
+        query.findObjectsInBackground { (objects, error) in
+            if error != nil {
+                self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+            } else {
+                if objects!.count > 0 {
+                    
+                    self.placeArray.removeAll(keepingCapacity: false)
+                    self.idArray.removeAll(keepingCapacity: false)
+                    
+                    for object in objects!{
+                        if let placeName = object.object(forKey: "name") as? String{
+                            if let placeId = object.objectId{
+                                self.placeArray.append(placeName)
+                                self.idArray.append(placeId)
+                            }
+                        }
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
     
     @objc func addButtonClicked(){
@@ -32,7 +89,6 @@ class PlacesVC: UIViewController {
             if error != nil {
                 self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
             } else {
-                
                 self.performSegue(withIdentifier: "toSignUpVC", sender: nil) 
             }
         }
